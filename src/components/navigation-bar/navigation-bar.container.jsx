d3ec0ca4cf470330ba4e6e2df1d55b9538e0af7d
Jared Cortez <jared.cortez@xref.com>
2020-05-05 22:41:49 +1000

    Material ui

---
+++ src/components/navigation-bar/navigation-bar.container.jsx
@@ -0,0 +1,9 @@
+import { connect } from 'react-redux';
+import { compose } from "recompose";
+
+import NavigationBar from "./navigation-bar.component";
+
+
+export default compose(
+   connect()
+)(NavigationBar);
---
+++ src/constants/themes.js
@@ -0,0 +1,12 @@
+import { createMuiTheme } from "@material-ui/core";
+
+export const theme = createMuiTheme({
+   palette: {
+      primary: {
+         main: '#A9F8FB'
+      },
+      secondary: {
+         main: '#B6DCFE'
+      }
+   }
+});
